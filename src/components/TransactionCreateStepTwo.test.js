153824a3ced90b09ad679fa2f8128484568c9e3c
Parmeet Singh <parmeet.asi@gmail.com>
2022-02-27 21:01:34 +0530

    add integration test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -9,7 +9,17 @@ test("on initial render, the pay button is disbaled", async () => {
 
 test("if an amount is entered the pay button is enabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "3" }} />);
-  // expect(await )
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50"); 
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
+
+test("integration test: if an amount is entered the pay button is enabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "3" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+
   userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
   userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
 
